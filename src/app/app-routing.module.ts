b31036a64cfa2bf0aaef5a2392546562cf9e474d
cesar <vcesar2021@gmail.com>
2023-11-10 00:42:35 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path:'heroes',
+    loadChildren: () => import('./heroes/heroes.module').then(m => m.HeroesModule)
+  },
+  {
+    path:'404',
+    component: Error404PageComponent
+  },
+  {
+    path:'',
+    redirectTo: 'heroes',
+    pathMatch:'full' // Nos aseguramos que redireccione al path exacto, en este caso heroes
+  },
+  {
+    path:'**',
+    redirectTo:'404',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
